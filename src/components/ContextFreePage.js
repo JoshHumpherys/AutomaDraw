102a8289d57de51102aa81f046a0f88bdedfc4d1
Josh Humpherys <joshua.humpherys@gmail.com>
2017-08-12 13:46:03 -0700

    Prepare for initial release

--- src/components/ContextFreePage.js
+++ src/components/ContextFreePage.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Button, Icon, Message } from 'semantic-ui-react'
 
 export class ContextFreePage extends Component {
   render() {
     return (
-      <div className="content-container">
-        <div className="control-panel-left">
-          left panel
-        </div>
-        <div className="center-container">
-        </div>
-        <div className="control-panel-right">
-          right panel
-        </div>
+      <div className="page-container">
+        <Message>
+          <Message.Header>
+            Context-free grammars are under construction!
+          </Message.Header>
+          <p>
+            We're actively working to implement all of the intended features.
+          </p>
+          <p>
+            The project is open source, so feel free to contribute!
+          </p>
+        </Message>
+        <Button onClick={() => window.open('https://github.com/JoshHumpherys/AutomaDraw')}>
+          <Icon name="github" size="big" className="clickable-icon" />GitHub!
+        </Button>
       </div>
     );
   }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Message } from 'semantic-ui-react'
+import { Button, Icon, Message } from 'semantic-ui-react'
 
 export class HomePage extends Component {
   render() {
@@ -11,9 +11,16 @@ export class HomePage extends Component {
             Welcome to AutomaDraw!
           </Message.Header>
           <p>
-            Here is some placeholder text that will eventually be a description of the application.
+            This is an application used to model and analyze finite automata and formal grammars.
+            It is currently under development, and as such is highly under-featured and may not work properly at times.
+          </p>
+          <p>
+            The project is open source, so feel free to contribute!
           </p>
         </Message>
+        <Button onClick={() => window.open('https://github.com/JoshHumpherys/AutomaDraw')}>
+          <Icon name="github" size="big" className="clickable-icon" />GitHub!
+        </Button>
       </div>
     );
   }

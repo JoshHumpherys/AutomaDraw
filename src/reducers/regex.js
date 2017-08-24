ad57d3a565e2cc5b8be374c26203ccdf0053aee4
Josh Humpherys <joshua.humpherys@gmail.com>
2017-08-24 18:52:26 -0400

    Refactor regex reducer

--- src/reducers/regex.js
+++ src/reducers/regex.js
@@ -1,18 +1,66 @@
+import { OrderedSet, Record } from 'immutable'
 import * as actionTypes from '../constants/actionTypes'
-import CreateRegex from '../objects/Regex'
 
-export default function regex(
-  state = {
-    regex: CreateRegex()
-  },
-  action) {
+export const symbolTypes = {
+  SYMBOL: 0,
+  EMPTY_STRING_SYMBOL: 1,
+  ALTERNATION_SYMBOL: 2
+};
+
+/*
+ * TODO add private keyword to functions that should not be exposed
+ * TODO encapsulate symbolTypes in Regex class
+ * TODO figure out a better way to get the regex string
+ * TODO use arrow functions
+ * TODO make static functions static
+ */
+class Regex extends Record({ regex: new OrderedSet() }) {
+  setRegexAction(regex, emptyStringSymbol, alternationSymbol) {
+    const symbolArray = [];
+    for(const symbol of regex) {
+      switch(symbol) {
+        case emptyStringSymbol:
+          symbolArray.push(this.createEmptyStringSymbol());
+          break;
+        case alternationSymbol:
+          symbolArray.push(this.createAlternationSymbol());
+          break;
+        default:
+          symbolArray.push(this.createSymbol(symbol));
+      }
+    }
+    return this.setRegex(new OrderedSet(symbolArray));
+  }
+
+  clearRegexAction() {
+    return this.setRegex(new OrderedSet());
+  }
+
+  setRegex(orderedSet) {
+    return this.set('regex', orderedSet);
+  }
+
+  createSymbol(symbol) {
+    return { symbolType: symbolTypes.SYMBOL, symbol };
+  }
+
+  createEmptyStringSymbol() {
+    return { symbolType: symbolTypes.EMPTY_STRING_SYMBOL };
+  };
+
+  createAlternationSymbol() {
+    return { symbolType: symbolTypes.ALTERNATION_SYMBOL };
+  }
+}
+
+export default function regex(state = new Regex(), action) {
   switch (action.type) {
     case actionTypes.REGEX_SET: {
       const { regex, emptyStringSymbol, alternationSymbol } = action.payload;
-      return { ...state, regex: state.regex.setRegex(regex, emptyStringSymbol, alternationSymbol) };
+      return state.setRegexAction(regex, emptyStringSymbol, alternationSymbol);
     }
     case actionTypes.REGEX_CLEARED: {
-      return { ...state, regex: state.regex.clearRegex() };
+      return state.clearRegexAction();
     }
     default: {
       return state;
--- src/selectors/regex.js
+++ src/selectors/regex.js
@@ -1,8 +1,22 @@
 import { getEmptyStringSymbol, getAlternationSymbol } from '../utility/utility'
+import { symbolTypes } from '../reducers/regex'
 
 export const getRegexString = state => {
-  return state.regex.regex.getRegexString(
-    getEmptyStringSymbol(state.settings.emptyStringSymbol),
-    getAlternationSymbol(state.settings.alternationSymbol)
-  );
+  const emptyStringSymbol = getEmptyStringSymbol(state.settings.emptyStringSymbol);
+  const alternationSymbol = getAlternationSymbol(state.settings.alternationSymbol);
+  let regexString = '';
+  for(const symbol of state.regex.regex) {
+    switch(symbol.symbolType) {
+      case symbolTypes.EMPTY_STRING_SYMBOL:
+        regexString += emptyStringSymbol;
+        break;
+      case symbolTypes.ALTERNATION_SYMBOL:
+        regexString += alternationSymbol;
+        break;
+      case symbolTypes.SYMBOL:
+      default:
+        regexString += symbol.symbol;
+    }
+  }
+  return regexString;
 };

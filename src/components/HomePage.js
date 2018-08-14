26bdea0139643826efd27cc4f20d45e2eec82770
Josh Humpherys <joshua.humpherys@gmail.com>
2018-08-13 22:47:37 -0700

    Update home page description

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,19 +12,19 @@ export class HomePage extends Component {
           </Message.Header>
           <p>
             This is an application used to model and analyze finite automata and formal grammars. It was initially
-            developed in 2017 by Josh Humpherys as part of his Senior Integration Project for Covenant College.
+            developed in 2017 by Josh Humpherys as part of his Senior Integration Project for Covenant College, but it
+            is still undergoing development.
           </p>
           <p>
-            The application currently supports modeling finite state machines, pushdown automata, and Turing machines.
-            Additionally, users can step through or run inputs on finite state machines and Turing machines.
-            Automata can be downloaded into special files so that they can be shared with others or uploaded later.
+            The application currently supports modeling and running finite state machines, pushdown automata, and Turing
+            machines. Automata can be downloaded into special files so that they can be shared with others or uploaded
+            later. Similarly, test cases can be created and downloaded so they can be shared or resused across sessions.
             There exists a simple regular expressions page that has buttons for special symbols such as the empty
             symbol, alternation symbol, and Kleene star, and there are settings to change these symbols,
             as well as to switch between a light theme and a dark theme.
           </p>
           <p>
-            Despite a substantial feature set, there is always more that can be added. The application does not yet
-            support running or stepping through inputs on Pushdown automata. Similarly, regular expressions can be
+            Despite a substantial feature set, there is always more that can be added. Regular expressions can be
             entered but not tested. There only exist stub pages for modeling grammars, and in the future it would be
             nice to be able to convert back and forth between grammars and automata. Also, the site performs poorly on
             mobile devices and is not compatible with touch screens. Nonetheless, the program is a great tool to model
